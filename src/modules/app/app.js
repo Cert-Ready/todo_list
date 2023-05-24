19efbc71eddc5003940748b6395e6dc74aa9f1e5
Cert Ready <120624638+Cert-Ready@users.noreply.github.com>
2023-05-24 16:42:42 -0500

    Add modal toggle onclick

--- src/modules/app/app.js
+++ src/modules/app/app.js
@@ -3,7 +3,35 @@ import Task from '../class/Task';
 import ProjectList from '../class/ProjectList';
 
 export function app() {
+  const addProjectBtn = document.querySelector('.add-project');
+  const modalContainer = document.querySelector('.modal');
+  const modal = document.querySelector('.formV01');
+
   preventDefault();
+
+  addProjectBtn.addEventListener('click', () => {
+    // display modal
+    openAddProject(modalContainer, modal);
+  });
+
+  // toggle modal depending on area clicked
+  modalContainer.addEventListener('click', (e) => {
+    const closeBtn = document.querySelector('.formV01-btn-close');
+
+    // close modal if clicked out of bounds
+    if (e.target === modalContainer || e.target === closeBtn) {
+      toggleModal(modalContainer);
+    }
+  });
+}
+
+function openAddProject(modalContainer, modal) {
+  // display modal
+  toggleModal(modalContainer);
+}
+
+function toggleModal(modalContainer) {
+  modalContainer.classList.toggle('hidden');
 }
 
 function preventDefault() {
