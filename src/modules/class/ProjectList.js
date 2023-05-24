f3aabed7db71add1ed78a0abdebeb3e3412b14b4
Cert Ready <120624638+Cert-Ready@users.noreply.github.com>
2023-05-24 12:05:15 -0500

    Add, push & delete methods to ProjectList

--- src/modules/class/ProjectList.js
+++ src/modules/class/ProjectList.js
@@ -1,3 +1,11 @@
 export default class ProjectList {
 	static _projectList = [];
+
+	static addProject(project) {
+		this._projectList.push(project);
+	}
+
+	static deleteProject(project) {
+		this._projectList.splice(this._projectList.indexOf(project), 1);
+	}
 }
