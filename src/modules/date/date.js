94b75cfe5db4cc48ee64544cb693592bcce11a7c
Cert Ready <120624638+Cert-Ready@users.noreply.github.com>
2023-05-23 19:19:36 -0500

    Add date & greeting to UI

---
+++ src/modules/date/date.js
@@ -0,0 +1,29 @@
+import format from 'date-fns/format';
+
+export default function date() {
+	const date = new Date();
+
+	displayDate(date);
+	displayGreeting(date);
+}
+
+function displayDate(date) {
+	document.querySelector('.day-num').textContent = format(date, 'd');
+	document.querySelector('.day').textContent = format(date, 'EEEE');
+	document.querySelector('.month-year').textContent = format(date, 'LLLL yyyy');
+}
+
+function displayGreeting(date) {
+	const hour = format(date, 'H');
+	const greetingEl = document.querySelector('.greeting');
+
+	if (hour < 12) {
+		greetingEl.textContent = 'Good morning!';
+	} else if (hour < 16) {
+		greetingEl.textContent = 'Good afternoon!';
+	} else if (hour < 20) {
+		greetingEl.textContent = 'Good evening!';
+	} else {
+		greetingEl.textContent = 'Welcome!';
+	}
+}
