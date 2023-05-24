5f2f69e630297731d659115176f78254e372939a
Cert Ready <120624638+Cert-Ready@users.noreply.github.com>
2023-05-24 16:32:08 -0500

    Prevent default button event

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import './css/style.css';
 import './css/normalize.css';
 import navToggle from './modules/mobileNav/mobileNav';
 import date from './modules/date/date';
+import { app } from './modules/app/app';
 
 navToggle();
 date();
+app();
--- src/modules/app/app.js
+++ src/modules/app/app.js
@@ -1,3 +1,12 @@
 import Project from '../class/Project';
 import Task from '../class/Task';
 import ProjectList from '../class/ProjectList';
+
+export function app() {
+  preventDefault();
+}
+
+function preventDefault() {
+  const modalButtons = document.querySelectorAll('.formV01-btn');
+  modalButtons.forEach((btn) => btn.addEventListener('click', (e) => e.preventDefault()));
+}
