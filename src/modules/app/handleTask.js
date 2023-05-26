9014e1fce0c23856dc618435a4068b5ad6e74962
Cert Ready <120624638+Cert-Ready@users.noreply.github.com>
2023-05-25 22:22:11 -0500

    Push task to project

--- src/modules/app/handleTask.js
+++ src/modules/app/handleTask.js
@@ -1,4 +1,5 @@
-import { preventDefault, toggleElement, resetModal } from './utility';
+import { preventDefault, toggleElement, resetModal, findProject, parseHtml } from './utility';
+import Task from '../class/Task';
 
 const handleTask = (() => {
   const projectTitleEl = document.querySelector('.project-title');
@@ -14,15 +15,44 @@ const handleTask = (() => {
 
   function openAddTask(modalContainer, modal) {
     const addTaskModal = document.querySelector('.formV01-add-task ');
-    const btnModalAdd = document.querySelector('.add-task-submit-btn ');
-    const btnModalClose = document.querySelector('.add-task-btn-close');
+    const btnAdd = document.querySelector('.add-task-submit-btn ');
+    const btnClose = document.querySelector('.add-task-btn-close');
+    const taskName = document.querySelector('#task-name');
+    const taskDesc = document.querySelector('#task-description');
+    const taskDate = document.querySelector('#task-date');
 
     // open modal
     toggleElement(modalContainer);
     toggleElement(addTaskModal);
 
+    // add project
+    btnAdd.onclick = () => {
+      const taskPrio = document.querySelector('input[name="priority"]:checked').value;
+
+      // check for empty fields description is optional
+      if (taskName.value === '') {
+        alert('Error: please enter task name');
+        return;
+      }
+
+      if (taskDate.value === '') {
+        alert('Error: please select date');
+        return;
+      }
+
+      // push task to project
+      findProject(projectTitleEl.textContent).element.pushTask(
+        new Task(parseHtml(taskName.value), parseHtml(taskDesc.value), taskDate.value, taskPrio)
+      );
+
+      // close modal
+      resetModal(modal);
+      toggleElement(modalContainer);
+      toggleElement(addTaskModal);
+    };
+
     // close modal
-    btnModalClose.onclick = () => {
+    btnClose.onclick = () => {
       resetModal(modal);
       toggleElement(modalContainer);
       toggleElement(addTaskModal);
